76aacdb067c47c5389a72681a573b838803a00f1
Guilhermevitor044 <162389771+Guilhermevitor044@users.noreply.github.com>
2025-10-21 19:33:34 -0300

    Adiciona tela básica de cadastro de cliente

---
+++ resources/views/clientes/create.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro de Cliente</title>
+</head>
+<body>
+    <h1>Cadastro de Cliente</h1>
+
+    <form action="#" method="POST">
+        <label>Nome:</label>
+        <input type="text" name="nome"><br><br>
+
+        <label>Email:</label>
+        <input type="email" name="email"><br><br>
+
+        <label>Telefone:</label>
+        <input type="text" name="telefone"><br><br>
+
+        <button type="submit">Cadastrar</button>
+    </form>
+</body>
+</html>
